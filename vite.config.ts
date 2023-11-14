e2b18fa2d26e13a745c76f0df4de4982558d3701
hatiwowo <hatiwowo@163.com>
2023-11-14 14:50:48 +0800

    fix: move

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [vue(), legacy()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@images": path.resolve(__dirname, "./src/assets/images"),
+        },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  }
-})
+    test: {
+        globals: true,
+        environment: "jsdom",
+    },
+});
